b48db9aa0b31d54b9b944bef63cf9cddf92c434e
Kevin Chen <kevinchen8223@gmail.com>
2014-03-06 00:24:46 -0800

    Added conenction stuff

--- app/src/main/java/com/testing/blogreader/MainListActivity.java
+++ app/src/main/java/com/testing/blogreader/MainListActivity.java
@@ -3,27 +3,47 @@ package com.testing.blogreader;
 import android.app.ListActivity;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainListActivity extends ListActivity {
 
     protected String[] mBlogPostTitles;
 	public static final int NUMBER_OF_POSTS = 20;
+	public static final String TAG = MainListActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
 
+		try {
+			URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=" + NUMBER_OF_POSTS);
+			HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
+			connection.connect();
+			int responseCode = connection.getResponseCode();
+			Log.i(TAG, "Code: " + responseCode);
+		} catch (MalformedURLException e) {
+			Log.e(TAG, "Exception caught: ", e);
+		} catch (IOException e) {
+			Log.e(TAG, "Exception caught: ", e);
+		} catch (Exception e) {
+			Log.e(TAG, "Exception caught: ", e);
+		}
+
 		Resources resources = getResources();
 		mBlogPostTitles = resources.getStringArray(R.array.android_names);
 
       	ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mBlogPostTitles);
       	setListAdapter(adapter);
-//        String message = getString(R.string.no_items);
-//        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+
     }
 
 
